82a9ba84ed55a7f205b4b51a14563a7992204434
Valquer Rafael <valquer.rafael@gmail.com>
2023-04-01 18:08:20 -0300

    first commit

---
+++ src/main/java/br/edu/ifpb/pweb2/pederneira/PederneiraApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.pederneira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PederneiraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PederneiraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifpb/pweb2/pederneira/PederneiraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.pederneira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PederneiraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
