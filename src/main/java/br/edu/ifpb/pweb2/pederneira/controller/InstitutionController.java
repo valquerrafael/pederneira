d6244760c37ce2481266272d63ed66833cc1d165
Valquer Rafael <valquer.rafael@gmail.com>
2023-04-12 15:11:12 -0300

    create base create and read features

--- src/main/java/br/edu/ifpb/pweb2/pederneira/controller/InstitutionController.java
+++ src/main/java/br/edu/ifpb/pweb2/pederneira/controller/InstitutionController.java
@@ -1,7 +1,7 @@
 package br.edu.ifpb.pweb2.pederneira.controller;
 
 import br.edu.ifpb.pweb2.pederneira.model.Institution;
-import br.edu.ifpb.pweb2.pederneira.service.InstitutionService;
+import br.edu.ifpb.pweb2.pederneira.repository.InstitutionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,70 +9,47 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/institution")
 public class InstitutionController {
 
     @Autowired
-    private InstitutionService service;
+    private InstitutionRepository repository;
 
-    @GetMapping("/register")
+    @GetMapping("/create")
     public String getRegisterForm(Institution institution, Model model) {
         model.addAttribute("institution", institution);
-        return "institution/register";
+        return "institution/create";
     }
 
-    @PostMapping("/register")
+    @PostMapping("/create")
     public String register(
         Institution institution,
         BindingResult result,
         RedirectAttributes attributes
     ) {
-        if (result.hasErrors())
-            return "home";
+        if (!result.hasErrors())
+            this.repository.save(institution);
 
-        this.service.register(institution);
-
-        attributes.addFlashAttribute(
-            "message",
-            institution.getName() + institution.getAcronym() + institution.getPhone()
-        );
-        return "redirect:/";
+        return "redirect:/home";
     }
 
-    @GetMapping("/{id}")
-    public String readOne(@PathVariable(value = "id") Integer id, Model model) {
-        model.addAttribute("institution", this.service.readOne(id));
-        return "home";
+    @GetMapping("/read/{id}")
+    public String readOne(@PathVariable(name = "id") Integer id, Model model) {
+        Institution institution = this.repository.findById(id).orElseThrow();
+
+        model.addAttribute("institution", institution);
+        return "institution/read";
     }
 
-    @PostMapping("/{id}")
-    public String update(
-        @PathVariable(value = "id") Integer id,
-        Institution institution,
-        RedirectAttributes attributes
-    ) {
-        this.service.update(id, institution);
+    @GetMapping("/read-all")
+    public String readAll(Model model) {
+        List<Institution> institutions = this.repository.findAll();
 
-        attributes.addFlashAttribute(
-            "message",
-            "Institution updated successfully"
-        );
-        return "home";
-    }
-
-    @DeleteMapping("/{id}")
-    public String delete(
-        @PathVariable(value = "id") Integer id,
-        RedirectAttributes attributes
-    ) {
-        this.service.delete(id);
-
-        attributes.addFlashAttribute(
-            "message",
-            "Institution deleted successfully"
-        );
-        return "home";
+        model.addAttribute("institutions", institutions);
+        return "institution/readall";
     }
 
 }
