d2cf2ce3d130b15fd11dbe127a24a840bb36e501
Felipe Targino <targino.felipe@academico.ifpb.edu.br>
2023-06-24 22:34:41 -0300

    consulta de declarações vencidas e a vencer

--- src/main/java/br/edu/ifpb/pweb2/pederneira/repository/EnrollmentRepository.java
+++ src/main/java/br/edu/ifpb/pweb2/pederneira/repository/EnrollmentRepository.java
@@ -2,12 +2,22 @@ package br.edu.ifpb.pweb2.pederneira.repository;
 
 import br.edu.ifpb.pweb2.pederneira.model.Enrollment;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Integer> {
 
     List<Enrollment> findBySemesterId(Integer id);
+
+    @Query("SELECT e FROM Enrollment e WHERE e.semester.end < current_date")
+    List<Enrollment> findExpiredEnrollments();
+
+    @Query("SELECT e FROM Enrollment e WHERE e.semester.end >= current_date and e.semester.end <= :endDate")
+    List<Enrollment> findEnrollmentsEndingSoon(@Param("endDate") LocalDate endDate);
+
 
 }
